8c57e4cf3200fa9168dc00e0c32277be3afe74cb
Denis <getto_spasitel@vk.com>
2026-06-09 22:43:21 +1000

    Create answer.py

---
+++ Number of days 📅/answer.py
@@ -0,0 +1,8 @@
+number_user = int(input())
+
+if number_user == 1 or number_user == 3 or number_user == 5 or number_user == 7 or number_user == 8 or number_user == 10 or number_user == 12:
+    print('31')
+elif number_user == 2:
+    print('28')
+else:
+    print('30')
