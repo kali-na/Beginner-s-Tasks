386ee67894149cd44e86477687e6a31288549c43
Denis <getto_spasitel@vk.com>
2026-06-09 18:36:27 +1000

    Create answer.py

---
+++ Password 🔒/answer.py
@@ -0,0 +1,7 @@
+first_password = input('Введите ваш пароль: ')
+second_password = input('Повторите пароль: ')
+
+if first_password == second_password:
+    print('Пароль принят')
+else:
+    print('Пароль не принят')
