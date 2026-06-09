5f9d638f1c6c925abfe6188f9f529a03bc5aafd1
Denis <getto_spasitel@vk.com>
2026-06-09 12:33:08 +1000

    Create answer.py

---
+++ Arithmetic operations 👨‍💻/answer.py
@@ -0,0 +1,5 @@
+first_num, second_num = int(input()), int(input())
+
+print(first_num, "+", second_num, "=", first_num + second_num)
+print(first_num, "-", second_num, "=", first_num - second_num)
+print(first_num, "*", second_num, "=", first_num * second_num)
