a600021fbf302f95b2f0ed475ccb715f23558e8b
Denis <getto_spasitel@vk.com>
2026-06-10 22:04:57 +1000

    Create answer.py

---
+++ Roman numerals/answer.py
@@ -0,0 +1,24 @@
+number_user = int(input())
+
+if number_user == 1:
+    print('I')
+elif number_user == 2:
+    print('II')
+elif number_user == 3:
+    print('III')
+elif number_user == 4:
+    print('IV')
+elif number_user == 5:
+    print('V')
+elif number_user == 6:
+    print('VI')
+elif number_user == 7:
+    print('VII')
+elif number_user == 8:
+    print('VIII')
+elif number_user == 9:
+    print('IX')
+elif number_user == 10:
+    print('X')
+else:
+    print('ошибка')
