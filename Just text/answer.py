0405e258b3efaddc81b747e7efaaa9f855746f84
Denis <getto_spasitel@vk.com>
2026-06-27 21:30:42 +1000

    Create answer.py

---
+++ Just text/answer.py
@@ -0,0 +1,19 @@
+#"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+p = '"Python'
+i = ' is'
+a = ' a'
+g = ' great'
+l = ' language!",'
+s = ' said'
+F = ' Fred.'
+I = ' "I'
+d = " don't"
+e = ' ever'
+r = ' remember'
+h = ' having'
+t = ' this'
+m = ' much'
+f = ' fun'
+b = ' before."'
+
+print(p + i + a + g + l + s + F + I + d + e + r + h + t + m + f + b)
