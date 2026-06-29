b253ff74a9642a1006cd6bf7580f07eda51c2267
Denis <getto_spasitel@vk.com>
2026-06-29 22:17:49 +1000

    Create answer.py

---
+++ Regular polygon 🔶/answer.py
@@ -0,0 +1,9 @@
+from math import *
+
+number_user_int = int(input())
+number_user_float = float(input())
+
+print( (number_user_int * (number_user_float ** 2)) / (4 * tan(pi / number_user_int)))
+
+
+
