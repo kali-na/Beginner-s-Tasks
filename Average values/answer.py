7fe5c7c9bd57ca57fd8a82c8c87477072aa582c3
Denis <getto_spasitel@vk.com>
2026-06-29 22:22:52 +1000

    Create answer.py

---
+++ Average values/answer.py
@@ -0,0 +1,12 @@
+from math import *
+
+
+number_one, number_two = float(input()), float(input())
+
+print((number_one + number_two) / 2)
+print(sqrt(number_one * number_two))
+print(((2 * number_one) * number_two) / (number_one + number_two))
+print(sqrt(((number_one ** 2) + (number_two ** 2)) / 2))
+
+
+
