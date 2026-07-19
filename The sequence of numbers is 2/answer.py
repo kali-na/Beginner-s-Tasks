e5b3f205852766b32ade9af1c3c16c84fbe2e11b
Denis <getto_spasitel@vk.com>
2026-07-19 12:11:51 +1000

    Create answer.py

---
+++ The sequence of numbers is 2/answer.py
@@ -0,0 +1,6 @@
+numberStart = int(input())
+numberFinish = int(input())
+
+for i in range(numberStart, numberFinish + 1):
+    if (i % 17 == 0) or (i % 10 == 9) or ((i % 3 == 0) and (i % 5 == 0)):
+        print(i)
